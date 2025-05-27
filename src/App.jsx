811e318ec05e51b27503a60c988ab34546b5ccde
Chinmoy94 <chinmoym66@gmail.com>
2025-05-27 15:26:16 +0530

    login button added

--- src/App.jsx
+++ src/App.jsx
@@ -1,40 +1,50 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
 
-function App() {
-
- const[counter,setCounter] = useState(6)
-  //let counter =15
-  const addValue = ()=>{
-    if(counter < 20)
-      {
-        setCounter(counter + 1);
-
-      }
-   // counter = counter + 1
-   // console.log("clicked",counter)
-    
-  };
-  const removeValue = ()=>{
-    if(counter > 0)
+const App = function() {
+  return React.createElement(
+    'div',
+    { className: 'login-container' },
+    React.createElement(
+      'form',
       { 
-        setCounter(counter -1);
+        className: 'login-form',
+        onSubmit: (e) => {
+          e.preventDefault();
+          // Handle form submission here
+        }
+      },
+      React.createElement(
+        'div',
+        { className: 'form-group' },
+        React.createElement('label', { htmlFor: 'username' }, 'Username'),
+        React.createElement('input', {
+          type: 'text',
+          id: 'username',
+          name: 'username',
+          required: true
+        })
+      ),
+      React.createElement(
+        'div',
+        { className: 'form-group' },
+        React.createElement('label', { htmlFor: 'password' }, 'Password'),
+        React.createElement('input', {
+          type: 'password',
+          id: 'password',
+          name: 'password',
+          required: true
+        })
+      ),
+      React.createElement(
+        'button',
+        { 
+          type: 'submit',
+          className: 'submit-button'
+        },
+        'Submit'
+      )
+    )
+  );
+};
 
-      }
-     
-    // console.log("chinmoy",counter)
-  };
-
-  return (
-    <>
-      <h1>Chinmoy</h1>
-      <h2>counter value:{counter}</h2>
-     <button onClick={addValue}>Add Value </button> 
-     <br />
-     <button onClick={removeValue}>new value</button>
-     <p>Footer:{counter}</p>
-    </>
-  )
-}
-
-export default App
+export default App;
